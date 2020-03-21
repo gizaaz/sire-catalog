79ff00975fb229e5bfb4c349359976ce5f4a584c
onemorefelix <onemorefelix@gmail.com>
2020-03-21 18:42:47 +0200

    update column images and add images

--- database/migrations/2020_03_15_122907_create_categories_table.php
+++ database/migrations/2020_03_15_122907_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements("category_id");
             $table->string("category_name");
-            $table->string("images");
+            $table->text("images")->nullable();
         });
     }
 
