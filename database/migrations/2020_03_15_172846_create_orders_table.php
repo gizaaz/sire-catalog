acefbf3aea196c02c1f2a518c8cf2d0ff5294e25
onemorefelix <55834786+onemorefelix@users.noreply.github.com>
2020-04-09 18:44:09 +0300

    Update 2020_03_15_172846_create_orders_table.php

--- database/migrations/2020_03_15_172846_create_orders_table.php
+++ database/migrations/2020_03_15_172846_create_orders_table.php
@@ -16,7 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements("id");
             $table->integer("product_id");
-            $table->string("user_name");
+            $table->string("user_name")->nullable();
             $table->string("phone")->nullable();
             $table->string("email")->nullable();
             $table->text("description")->nullable();
